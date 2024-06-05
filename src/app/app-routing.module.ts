5c6de054b7c1ba520331c876cc55733275aa5d88
reza razjoo <reza.razjoo@yahoo.com>
2024-06-05 23:30:20 +0100

    counter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 const routes: Routes = [
-  {path:'tasks', component: TasksComponent}
+  {path:'tasks', component: TasksComponent},
+  {path: 'counter', component:MyCounterComponent}
 ];
 
 @NgModule({
